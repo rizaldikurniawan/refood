ed5c42969e123cadf44108da2ce059332a7fd163
MIAbidin <muhammadirfanabidin@gmail.com>
2024-06-07 10:03:00 +0700

    home page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
+import 'regenerator-runtime';
+
 import { createCard } from './components/CardComponent';
 import { createJumbotron } from './components/JumbotronComponent';
 import '../styles/main.css';
