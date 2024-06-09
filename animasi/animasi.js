0197241c166f82aee0b2156a78ca4657cfd26dd3
Rizky Rizaldi Kurniawan <rizaldi410@gmail.com>
2024-06-09 21:47:54 +0800

    Animasi Hampir Selesai (kurang dibagian gerakan lebih mulus)

--- animasi/animasi.js
+++ animasi/animasi.js
@@ -1,41 +1,47 @@
 gsap.to('#imageBanner', { duration: 2, x: 200 });
 
 gsap.to('#logo-detail', {
-    rotation: 360,
-    x: '42.6vw',
-    xPercent: -42.6,
-    duration: 2,  
-    repeat: 2,    
-    yoyo: true,   
-    onComplete: function() {
-      gsap.to('#logo-detail', {
-        duration: 2,
-        rotation: 0,
-        x: 0,
-        xPercent: 0,
-        ease: "power1.inOut"
-      });
-    }
-  });
+  rotation: 360,
+  x: '42.6vw',
+  xPercent: -42.6,
+  duration: 2,
+  repeat: 2,
+  yoyo: true,
+  onComplete: function () {
+    gsap.to('#logo-detail', {
+      duration: 2,
+      rotation: 0,
+      x: 0,
+      xPercent: 0,
+      ease: 'power1.inOut',
+    });
+  },
+});
 
-  gsap.to('.judul', { duration: 2, x: 450 });
+gsap.to('.judul', { duration: 2, x: 450 });
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const bannerImage = document.getElementById("imageBanner");
-    const bannerTitle = document.getElementById("judul");
-    const bannerDescription = document.getElementById("description").querySelector("p");
+document.addEventListener('DOMContentLoaded', function () {
+  const bannerImage = document.getElementById('imageBanner');
+  const bannerTitle = document.getElementById('judul');
+  const bannerDescription = document.getElementById('description').querySelector('p');
+  const bannerDescriptionTeks = document.getElementById('description').querySelector('h2');
 
-    const carouselItems = document.querySelectorAll(".carousel-item");
+  const carouselItems = document.querySelectorAll('.item');
 
-    carouselItems.forEach(item => {
-      item.addEventListener("mouseover", function () {
-        const imageSrc = item.getAttribute("data-image");
-        const title = item.getAttribute("data-title");
-        const description = item.getAttribute("data-description");
+  carouselItems.forEach((item) => {
+    item.addEventListener('mouseover', function () {
+      const imageSrc = item.getAttribute('data-image');
+      const title = item.getAttribute('data-title');
+      const descriptionP = item.getAttribute('data-description');
+      const descriptionTeks = item.getAttribute('data-title');
 
-        bannerImage.src = imageSrc;
-        bannerTitle.textContent = title;
-        bannerDescription.textContent = description;
-      });
+      
+      bannerImage.src = imageSrc;
+      bannerTitle.textContent = title;
+
+      bannerDescriptionTeks.textContent = descriptionTeks;
+
+      bannerDescription.textContent = descriptionP;
     });
   });
+});
