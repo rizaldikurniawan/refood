6d4887b84b9edcacf5256b418735f81e839fa8e5
Kelvin Jaya Pratama <kevin.pratama301@gmail.com>
2024-06-09 20:54:37 +0700

    index

--- src/script/index.js
+++ src/script/index.js
@@ -10,7 +10,11 @@ document.addEventListener("DOMContentLoaded", function () {
         if (responseJson.error) {
           showMessageError(responseJson.message);
         } else {
+<<<<<<< HEAD
+          renderRefood(responseJson.data);
+=======
           // renderRefood(responseJson.data);
+>>>>>>> 0197241c166f82aee0b2156a78ca4657cfd26dd3
           console.log(responseJson.data);
         }
       })
@@ -21,3 +25,40 @@ document.addEventListener("DOMContentLoaded", function () {
 
   getRefood();
 });
+<<<<<<< HEAD
+
+function renderRefood(refoods) {
+  const carouselActiveContainer = document.querySelector(".carousel-item");
+  carouselActiveContainer.innerHTML = "";
+
+  const carouseRow = document.createElement("div");
+  carouseRow.classList.add("row");
+
+  refoods.forEach(refoods => {
+    const carouselHTML = makeCarousel(refoods);
+    carouseRow.appendChild(carouselHTML);
+  });
+
+  carouselActiveContainer.appendChild(carouseRow);
+
+
+};
+
+
+  function makeCarousel(refoods) {
+  
+    // Membuat HTML untuk elemen carousel
+    const carouselHTML = `
+      <div class="col-md-4 item">
+        <img src="${baseUrl}/img/${refoods.gambar}" class="d-block w-100 gambar" alt="${refoods.jenis}">
+        <div class="carousel-caption d-none d-md-block">
+          <h5 class="judul-item">${refoods.jenis}</h5>
+          <a href="detail.html" class="btn btn-primary" id="cara-pengolahan">Cara Pengolahan</a>
+        </div>
+      </div>
+    `;
+  
+    return carouselHTML;
+  };
+=======
+>>>>>>> 0197241c166f82aee0b2156a78ca4657cfd26dd3
