ac8633597572a40eb5cfc30e92325448f4484253
Rizky Rizaldi Kurniawan <rizaldi410@gmail.com>
2024-06-10 21:36:16 +0800

    About us (Poto Profil Belum Tampil)

--- src/scripts/views/pages/about-us.js
+++ src/scripts/views/pages/about-us.js
@@ -1,13 +1,60 @@
 const About = {
-    async render() {
-      return `
-        <h2>About Us Page</h2>
+  async render() {
+    return `
+        <div id="about">
+        <article>
+          <h1 class="judul">Apa itu Website Refood?</h1>
+          <div class="informasi">
+              <p>Website ReFood (Recycle Food) menyediakan informasi yang bermanfaat dan cukup lengkap mengenai cara mengolah dan mendaur ulang limbah makanan guna mengurangi pemborosan makanan.</p>
+              <ul>
+                <p><b>Fitur - fitur pada website:</b></p>
+                <li>Mencari informasi tentang limbah makanan</li>
+                <li>Menampilkan bagaimana cara mengolah limbah makanan</li>
+                <li>Pengguna dapat menambahkan informasi baru tentang bagaimana cara mengolah limbah makanan</li>
+              </ul>
+          </div>
+        </article>
+        <article>
+          <div class="informasi" >
+          <h2>Tim Kami</h2>
+          <div class="row">
+            <div class="col-sm-4">
+              <figure>
+                <img id="rizky" class="image-profil" alt="Foto profil Rizky Rizaldi Kurniawan" />
+                <figcaption>
+                  <p>Rizky Rizaldi Kurniawan</p>
+                  <p>Universitas Negeri Malang</p>
+                </figcaption>
+              </figure>
+            </div>
+            <div class="col-sm-4">
+              <figure>
+                <img class="image-profil" alt="Foto profil Kelvin Jaya Pratama" />
+                <figcaption>
+                  <p>Kelvin Jaya Pratama</p>
+                  <p>Universitas AMIKOM Yogyakarta</p>
+                </figcaption>
+              </figure>
+            </div>
+            <div class="col-sm-4">
+              <figure>
+                <img id="irfan" class="image-profil" alt="Foto profil Muhammad Irfan Abidin" />
+                <figcaption>
+                  <p>Muhammad Irfan Abidin</p>
+                  <p>Universitas Muhammadiyah Surakarta</p>
+                </figcaption>
+              </figure>
+            </div>
+          </div>
+        </div>
+        </article>
+      </div>
       `;
-    },
-   
-    async afterRender() {
-      // Fungsi ini akan dipanggil setelah render()
-    },
-  };
-   
-  export default About;
+  },
+
+  async afterRender() {
+    // Fungsi ini akan dipanggil setelah render()
+  },
+};
+
+export default About;
--- webpack.dev.js
+++ webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
   devServer: {
     static: path.resolve(__dirname, 'dist'),
     open: true,
-    port: 9000,
+    port: 3000,
     client: {
       overlay: {
         errors: true,
